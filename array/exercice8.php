f23c19beacb25081ccdc1c6c4b9601f80e97e57d
tsague <tsagueymele@yahoo.fr>
2017-01-11 12:04:56 +0100

    Array exercice 8 terminé

---
+++ array/exercice8.php
@@ -0,0 +1,9 @@
+<?php
+
+$arrayMonth = array("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
+
+foreach ($arrayMonth as $value) {
+	echo $value.'<br>';
+}
+
+?>
