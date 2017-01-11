2ab7bb85bd7d02501c5aa648fc6cdd5a7bc6c4c5
tsague <tsagueymele@yahoo.fr>
2017-01-11 11:51:19 +0100

    Array Exercice 5 terminé

---
+++ array/exercice5.php
@@ -0,0 +1,12 @@
+<?php
+
+$arrayDepHautFrance = array(
+	02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise',
+    62 => 'Pas de Calais',
+    80 => 'Somme',
+);
+var_dump($arrayDepHautFrance);
+
+?>
