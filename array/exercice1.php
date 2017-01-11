e7db2f312e8c511f97793ce23273039536203c62
tsague <tsagueymele@yahoo.fr>
2017-01-11 10:25:38 +0100

    Array exercice 1

---
+++ array/exercice1.php
@@ -0,0 +1,18 @@
+<?php
+$arrayMonth = array(
+	 1 => 'Janvier',
+     2 => 'Février',
+     3 => 'Mars',
+     4 => 'Avril',
+     5 => 'Mai',
+     6 => 'Juin',
+     7 => 'Juillet',
+     8 => 'Août',
+     9=> 'Septembre',
+    10=> 'Octobre',
+    11 => 'Novembre',
+    12 => 'Décembre'
+);
+var_dump($arrayMonth); 
+
+?>
