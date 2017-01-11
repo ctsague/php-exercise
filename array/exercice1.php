51e73dc1bb1b831c32d0ebcfaefcd3b64e3f03f1
tsague <tsagueymele@yahoo.fr>
2017-01-11 11:46:16 +0100

    Array exercice 4 avec tableau sans index predefini

--- array/exercice1.php
+++ array/exercice1.php
@@ -1,20 +1,10 @@
 <?php
-$arrayMonth = array(
-	 0 => 'Janvier',
-     1 => 'Février',
-     2 => 'Mars',
-     3 => 'Avril',
-     4 => 'Mai',
-     5 => 'Juin',
-     6 => 'Juillet',
-     7 => 'Août',
-     8=> 'Septembre',
-     9=> 'Octobre',
-    10 => 'Novembre',
-    11 => 'Décembre'
-);
-// remplacer la valeur de Août par Aout
-$arrayMonth[7] ='Aout';
+//autre facon de faire un tableau
+$arrayMonth = array("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
+
+var_dump($arrayMonth);
+
+$arrayMonth[7] ='août';
 echo $arrayMonth[7];
 
 ?>
