396269f597332bacbd6e61c036ce4d7036baa13c
tsague <tsagueymele@yahoo.fr>
2017-01-11 12:14:29 +0100

    Array exercice 9 terminé

---
+++ array/exercice9.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrayDepHautFrance = array(
+	02 => 'Aisne',
+    59 => 'Nord',
+    60 => 'Oise',
+    62 => 'Pas de Calais',
+    80 => 'Somme',
+);
+
+foreach ($arrayDepHautFrance as $key => $value) {
+	echo $value. '<br>';
+	//echo "Clé : $key; Valeur : $value<br />";
+}
+?>
