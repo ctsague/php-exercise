4ac1a7321d78e2783075143e3fe6e0ae7d00a7cb
tsague <tsagueymele@yahoo.fr>
2017-01-12 10:26:53 +0100

    ParamUrl ecercice 1 revu

--- paramUrl/exercice1/index.php
+++ paramUrl/exercice1/index.php
@@ -3,7 +3,7 @@
 if(isset($_GET['nom']) && isset($_GET['prenom'])) {
 	echo 'Bonjour '. $_GET['nom']. ' '.$_GET['prenom'];
 } else {
-	echo "Entrer un nom et/ou prenom valide";
+	echo "Pas de nom ou de prenom défini";
 }
 
 ?>
